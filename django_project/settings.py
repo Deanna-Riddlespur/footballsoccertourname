9fc4f1c28e467b41897cc2d914166ad9c214a265
DeAnna Lynn Riddlespur <webdevdea@gmail.com>
2015-08-05 19:36:36 -0500

    add pass

--- django_project/settings.py
+++ django_project/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django',
         'USER': 'django',
-        'PASSWORD': 'HOjqPeQWnb',
+        'PASSWORD': 'VI9wXjZggo',
         'HOST': 'localhost',
         'PORT': '',
     }
