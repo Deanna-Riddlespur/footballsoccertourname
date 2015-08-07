bbad39bc7302abd184328c12e50ee2906a9460ce
DeAnna Lynn Riddlespur <webdevdea@gmail.com>
2015-08-07 18:41:53 -0500

    add url

--- django_project/urls.py
+++ django_project/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^soccer-api/', include(soccer_urls)),
     url(r'^football-api/', include(football_urls)),
+    url(r'^tournament-api/', include(football_urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 
 ]
